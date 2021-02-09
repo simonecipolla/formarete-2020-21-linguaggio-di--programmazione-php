9d15f71129b6b47a772b624567683cc0e3c3c419
corsidrive <corsidrive@gmail.com>
2021-02-09 11:33:54 +0100

    stringhe in ph

---
+++ string_basic/literal_string.php
@@ -0,0 +1,9 @@
+<?php
+$nome = "Mirellla";
+// single quote
+echo 'Ciao mi chiamo $nome \n';
+echo "\n";
+// double quote
+echo "Ciao mi chiamo $nome \n";
+echo "$nome ha detto: \" dammi \$5 e  1\\4 \" ";
+echo "\n$nome è composto da " . strlen($nome) . ' lettere';
