4a91e590d5659d08d2d3e4de70d632c2ada43f26
ryoga <sarassoroberto@gmail.com>
2021-02-25 13:28:27 +0100

    file_get_content

---
+++ case_study/tasklistArray/file_get_contents.php
@@ -0,0 +1,9 @@
+<?php 
+
+$jsonString = file_get_contents('./dataset/TaskList.json');
+$jsonArray = json_decode($jsonString, true);
+// var_dump($jsonString);
+
+//echo gettype($jsonString);
+
+print_r($jsonArray[17]['expirationDate']);
